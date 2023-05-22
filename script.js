369b0dfc589f75f472a8d1e428baf48f1d553337
NazariiIhnat <ignatnazar@gmail.com>
2023-05-22 22:23:16 +0200

    Refactor passing object to setDataToWeatherCard method. Created field of 3 hour 5 days fweather forecast request.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const weatherApiKey = "4994c6d047fcd34481fdda640bf387f2";
 const cityAutocompleteApiKey = "46285a33571b4253b2b9c5ac3d9e2a95";
-const weatherRequest = `https://api.openweathermap.org/data/2.5/weather?appid=${weatherApiKey}&units=metric`;
+const currrentWeatherRequest = `https://api.openweathermap.org/data/2.5/weather?appid=${weatherApiKey}&units=metric`;
+const hourlyWeatherForecastReqiest = `https://api.openweathermap.org/data/2.5/forecast?appid=${weatherApiKey}&ctn=40&units=metric`;
 const reverseGeolocationRequest = `http://api.openweathermap.org/geo/1.0/reverse?appid=${weatherApiKey}`;
 const cityAutocompleteRequest = `https://api.geoapify.com/v1/geocode/autocomplete?apiKey=${cityAutocompleteApiKey}&type=city&text=`;
 const searchEl = document.querySelector(".search");
@@ -31,44 +32,55 @@ searchEl.addEventListener("keypress", function (e) {
   if (e.key === "Enter") setWeather();
 });
 
-async function setWeather(city) {
-  let cityName;
-  city ? (cityName = city) : (cityName = searchEl.querySelector("input").value);
-  const data = await getWeatherOfCity(cityName);
+async function setWeather(cityName) {
+  let theCity;
+  cityName
+    ? (theCity = cityName)
+    : (theCity = searchEl.querySelector("input").value);
+  const data = await getWeatherOfCity(theCity);
   if (!data.weather) {
     alert(data.message);
     return;
   }
-  setDataToWeatherCard(data);
+  const { icon: iconID, description: weatherDescription } = data.weather[0];
+  const { temp, humidity } = data.main;
+  const { name: city } = data;
+  const country = data.sys.country;
+  const { deg: windDirection, speed: windSpeed } = data.wind;
+  const clouds = data.clouds.all;
+  const weatherObj = {
+    iconID,
+    weatherDescription,
+    temp,
+    humidity,
+    city,
+    country,
+    windDirection,
+    windSpeed,
+    clouds,
+  };
+  setDataToWeatherCard(weatherObj);
 }
 
 async function getWeatherOfCity(city) {
-  const response = await fetch(`${weatherRequest}&q=${city}`);
+  const response = await fetch(`${currrentWeatherRequest}&q=${city}`);
   return response.ok ? response.json() : new Error(`City ${city} not found!`);
 }
 
 function setDataToWeatherCard(data) {
-  const iconID = data.weather[0].icon;
-  const weatherDescription = data.weather[0].description;
-  const temp = Math.round(+data.main.temp);
-  const city = data.name;
-  const country = data.sys.country;
-  const windSpeed = data.wind.speed;
-  const windDirection = data.wind.deg;
-  const humidity = data.main.humidity;
-  const clouds = data.clouds.all;
-
-  document.querySelector(".weather .icon img").src = `icon/${iconID}.png`;
+  document.querySelector(".weather .icon img").src = `icon/${data.iconID}.png`;
   document.querySelector(".weather .description").textContent =
-    weatherDescription;
-  document.querySelector(".temp .value").textContent = temp;
+    data.weatherDescription;
+  document.querySelector(".temp .value").textContent = data.temp;
   document.querySelector(
     ".weather .location"
-  ).textContent = `${city}, ${country}`;
-  document.querySelector(".wind .value").textContent = windSpeed;
+  ).textContent = `${data.city}, ${data.country}`;
+  document.querySelector(".wind .value").textContent = data.windSpeed;
   document.querySelector(".wind .arrow").style.transform = `rotate(${
-    windDirection > 180 ? -Math.abs(360 - windDirection) : windDirection
+    data.windDirection > 180
+      ? -Math.abs(360 - data.windDirection)
+      : data.windDirection
   }deg)`;
-  document.querySelector(".humidity .value").textContent = humidity;
-  document.querySelector(".clouds .value").textContent = clouds;
+  document.querySelector(".humidity .value").textContent = data.humidity;
+  document.querySelector(".clouds .value").textContent = data.clouds;
 }
